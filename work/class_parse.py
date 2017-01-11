9dc6a328b9bacb41952dbb6fde7ab861b94e4b5a
saashimi <saavedra.kevin@gmail.com>
2017-01-11 10:03:39 -0800

    Success with xlist merge

--- work/class_parse.py
+++ work/class_parse.py
@@ -5,19 +5,30 @@ import datetime
 #pd.options.mode.chained_assignment = None
 current_term = 201604
 
-def aggregate(df_in):
-# Aggregate arithmetic operations:
-    operations = {'Weekly_Class_Hours' : 'sum', '%_Capacity' : 'mean', 'Actual_Enrl' : 'mean'}
-    df_in.groupby('Meeting_Location_1', as_index=False).agg(operations)
+def aggregate(df_agg):
+    # Aggregate arithmetic operations:
+    operations = ({'Weekly_Class_Hours' : 'sum', 
+                   '%_Capacity' : 'mean', 
+                   'Actual_Enrl' : 'mean'})
+    df_agg.groupby('Meeting_Location_1', as_index=False).agg(operations)
 
     # Calculate hourly utilizations
-    df_in['Class_Hour_Utilization'] = (df_in['Weekly_Class_Hours']/40.0).astype(float)
+    df_agg['Class_Hour_Utilization'] = (df_agg['Weekly_Class_Hours']/40.0).astype(float)
 
     # Round optimal size to the nearest 5
-    df_in['Optimal_Size'] = 5 * round((df_in['Actual_Enrl'] * 1.25)/5)
+    df_agg['Optimal_Size'] = 5 * round((df_agg['Actual_Enrl'] * 1.25)/5)
     # Optimal size should be a minimum of 10 seats
-    df_in.loc[df_in['Optimal_Size'] < 10.0, 'Optimal_Size'] = 10.0
-    return df_in
+    df_agg.loc[df_agg['Optimal_Size'] < 10.0, 'Optimal_Size'] = 10.0
+    return df_agg
+
+def merge_xlist(df_xl):
+    # merge the crosslised classes
+    xl_operations = ({'Actual_Enrl' : 'sum', 
+                          'Room_Capacity' : 'max'})
+    df_xl = df_xl.groupby('Xlst', as_index=False).agg(xl_operations)
+    df_xl['%_Capacity'] = df_xl['Actual_Enrl'] / df_xl['Room_Capacity'].astype(int)
+    #df_xl.drop('Xlst_Max_Enrl', axis=1, inplace=True)
+    return df_xl
 
 
 df = pd.read_csv('classroom_data/PSU_master_classroom.csv')
@@ -58,12 +69,15 @@ for row in df['Days']:
     else:
         df['Days_Per_Week'] = df['Days'].str.len()
 
-df['%_Capacity'] = df['Actual_Enrl'].astype(float) / df['Max_Enrl'].astype(float) 
+df['%_Capacity'] = df['Actual_Enrl'].astype(int) / df['Room_Capacity'].astype(int) 
 df['Actual_Enrl'] = df['Actual_Enrl'].astype(int)
 
-x_list_operations = {'Actual_Enrl' : 'sum', 'Xlst_Max_Enrl' : 'max'}
-df_xlist = df.groupby('Xlst', as_index=False).agg(x_list_operations)
+### This is a useful lambda example
+#df['Xlst_Max_Enrl'] = df['Xlst_Max_Enrl'].apply(lambda x: x if (x != '') else '0')
+####
+
+df_xlist = merge_xlist(df)
 print(df_xlist)
 
 
-print(df[['Class', 'ROOM', '%_Capacity', 'Xlst', 'Start_Date', 'End_Date', 'Days', 'Actual_Enrl', 'Duration_Hr']])
+#print(df[['Class', 'ROOM', '%_Capacity', 'Xlst', 'Start_Date', 'End_Date', 'Days', 'Actual_Enrl', 'Duration_Hr']])
